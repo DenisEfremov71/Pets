b00ece3c26a9b190ef49bf14d7193b52e71796f6
Denis Efremov <efremov@novuscom.net>
2023-10-18 17:37:56 -0700

    Initial Commit

---
+++ Pets/PetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetsApp.swift
+//  Pets
+//
+//  Created by Denis Efremov on 2023-10-18.
+//
+
+import SwiftUI
+
+@main
+struct PetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
